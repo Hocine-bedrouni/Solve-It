fae4374b413973277b8a35ef951ab3cc35672fea
roumaissa OURABAH <er_ourabah@esi.dz>
2022-07-08 16:28:51 +0200

    service

--- src/main/java/fr/solveit/controller/AbsenceController.java
+++ src/main/java/fr/solveit/controller/AbsenceController.java
@@ -1,9 +1,57 @@
 package fr.solveit.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fr.solveit.model.Absence;
+import fr.solveit.model.AbsenceId;
+import fr.solveit.model.Calendrier;
+import fr.solveit.model.CalendrierId;
+import fr.solveit.service.AbsenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/absences")
 public class AbsenceController {
+    @Autowired
+    private AbsenceService absenceService;
+
+    @GetMapping("")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Absence> findAll() {return absenceService.findAll();}
+
+    @GetMapping("/{id}")
+    @ResponseStatus(code= HttpStatus.OK)
+    public Absence findById(@PathVariable AbsenceId id) {
+        return absenceService.findById(id);
+    }
+
+    @GetMapping("/{numSec}")
+    @ResponseStatus(code= HttpStatus.OK)
+    public List<Absence> findByStagiaire(@PathVariable String numSec) {
+        return absenceService.findByStagiaire(numSec);
+    }
+    
+    @PostMapping("")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Absence create(@RequestBody Absence absence) {
+        return absenceService.create(absence);
+    }
+
+    @PostMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public Absence update(@RequestBody Absence absence,@PathVariable AbsenceId id) {
+        if (!id.equals(absence.getAbsenceId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "mauvais Absence a mettre à jour");
+        }
+        return absenceService.update(absence);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code=HttpStatus.ACCEPTED)
+    public Absence delete(@PathVariable AbsenceId id) {
+        return absenceService.delete(id);
+    }
 }
--- src/main/java/fr/solveit/repository/ISituationProfessionnelleRepo.java
+++ src/main/java/fr/solveit/repository/ISituationProfessionnelleRepo.java
@@ -1,7 +1,6 @@
 package fr.solveit.repository;
 
 import fr.solveit.model.SituationProrfessionnelle;
-import fr.solveit.model.Ville;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
